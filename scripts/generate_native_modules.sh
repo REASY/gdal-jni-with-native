a119e31773c6b08025548d009f17eeadf693689b
Artavazd Balaian <reasyu@gmail.com>
2023-08-24 07:41:39 +0800

    chore: Update GDAL to 3.6.4

--- scripts/generate_native_modules.sh
+++ scripts/generate_native_modules.sh
@@ -15,22 +15,20 @@ esac
 
 export SCRIPT_DIR
 
-PROJ_VERSION="9.1.1"
-GDAL_VERSION="3.5.3"
+PROJ_VERSION="9.2.1"
+GDAL_VERSION="3.6.4"
 
 # Build GDAL
 "${SCRIPT_DIR}/build_gdal.sh" $PROJ_VERSION $GDAL_VERSION
 
-docker run -it --entrypoint /bin/bash -u $(id -u):$(id -g) \
- -v $(pwd)/libs:/java_libs \
+docker run -it --entrypoint /bin/bash -v $(pwd)/libs:/java_libs \
  qartez-engine/gdal:"$GDAL_VERSION" -c "cp /usr/share/java/gdal-*.jar /java_libs/"
 
 # Build lddtopo
 "${SCRIPT_DIR}/build_lddtopo.sh" $GDAL_VERSION
 
 # Analyze dependencies, build DAG, run toposort and copy them to /resource folder
-docker run -u $(id -u):$(id -g) \
-  -v $(pwd)/src/main/resources/:/resources qartez-engine/lddtopo:"$GDAL_VERSION" /usr/share/java/libgdalalljni.so /resources
+docker run -v $(pwd)/src/main/resources/:/resources qartez-engine/lddtopo:"$GDAL_VERSION" /usr/share/java/libgdalalljni.so /resources
 
 # Build and test GDAL native library
 lib_name=$(./gradlew properties | grep ^name | sed 's/name: //g')
